a2998ddc77977ba54705f97ab92447f87eac7ae2
Max <contact@slymax.com>
2022-07-14 19:09:29 +0200

    remove deno wrapper

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if ! command -v deno &> /dev/null
-then
-    echo 'Please make sure that Deno is installed and added to your $PATH before installing Chomp'
-    echo 'https://deno.land/#installation'
-    exit
-fi
-deno install --allow-all --no-check --unstable --reload --force https://cdn.jsdelivr.net/gh/slymax/chomp/chomp.js
--- readme.md
+++
@@ -1,3 +0,0 @@
-```
-curl -s https://cdn.jsdelivr.net/gh/slymax/chomp/install.sh | sh
-```
